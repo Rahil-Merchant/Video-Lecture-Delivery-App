2288d6c2f486edc2a2ccebf535eb06166720fe8a
Akash Dubey <akash1999dubey@outlook.com>
2019-06-16 18:07:20 +0530

    Video Player worked for Vimeo Video, but for a random video

--- app/src/main/java/com/example/gumptionlabs/homeActivity.java
+++ app/src/main/java/com/example/gumptionlabs/homeActivity.java
@@ -1,9 +1,13 @@
 package com.example.gumptionlabs;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,6 +21,8 @@ public class homeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -34,4 +40,34 @@ public class homeActivity extends AppCompatActivity {
             }
         });
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:{
+                    Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                case R.id.navigation_dashboard: {
+                    Toast.makeText(getApplicationContext(), "DashBoard", Toast.LENGTH_SHORT).show();
+                     Intent i = new Intent(homeActivity.this, VideoPlayer.class);
+//                    val intent = Intent(this, AnotherActivity::class.java)
+                    startActivity(i);
+                    return true;
+                }
+                case R.id.navigation_notifications: {
+                    Toast.makeText(getApplicationContext(), "Notifications", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                case R.id.navigation_settings: {
+                    Toast.makeText(getApplicationContext(), "Video Player", Toast.LENGTH_SHORT).show();// Set to video player for now
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
 }

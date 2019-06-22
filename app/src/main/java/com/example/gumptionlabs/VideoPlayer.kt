585c78cca35ccbf2897c1b5ccbdb34502b13761e
Akash Dubey <akash1999dubey@outlook.com>
2019-06-22 16:45:03 +0530

    Video Player is integrated.

--- app/src/main/java/com/example/gumptionlabs/VideoPlayer.kt
+++ app/src/main/java/com/example/gumptionlabs/VideoPlayer.kt
@@ -2,19 +2,22 @@ package com.example.gumptionlabs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_video_player.*
-//import com.ct7ct7ct7.androidvimeoplayersample.R
+
 
 class VideoPlayer  : AppCompatActivity(){
-
+    var number = 59777392
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
         setupView()
+        Log.d("DEBUG","HELP")
     }
-
     private fun setupView() {
-        //lifecycle.addObserver(vimeoPlayer)
-        //vimeoPlayer.initialize(59777392)
+        lifecycle.addObserver(vimeoPlayer)
+        number = Integer.parseInt(intent.getStringExtra("myvideoURL"))
+        Log.d("TAG",""+number)
+        vimeoPlayer.initialize(number)//59777392)
     }
 }

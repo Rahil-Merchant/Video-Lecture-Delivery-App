bb09763a4108c32a79b9a91186594792f86a9527
Akash Dubey <akash1999dubey@outlook.com>
2019-07-27 22:04:06 +0530

    Fixed Vid Player( Added Filter)

--- app/src/main/java/com/example/gumptionlabs/VideoPlayer.kt
+++ app/src/main/java/com/example/gumptionlabs/VideoPlayer.kt
@@ -1,23 +1,59 @@
+
 package com.example.gumptionlabs
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.ct7ct7ct7.androidvimeoplayer.model.PlayerState
+import com.ct7ct7ct7.androidvimeoplayer.view.VimeoPlayerActivity
 import kotlinx.android.synthetic.main.activity_video_player.*
 
 
 class VideoPlayer  : AppCompatActivity(){
+    var REQUEST_CODE = 1234
     var number = 59777392
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
+        setupToolbar()
         setupView()
-        Log.d("DEBUG","HELP")
     }
+
+    private fun setupToolbar() {
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
     private fun setupView() {
         lifecycle.addObserver(vimeoPlayer)
         number = Integer.parseInt(intent.getStringExtra("myvideoURL"))
         Log.d("TAG",""+number)
         vimeoPlayer.initialize(number)//59777392)
+        vimeoPlayer.setFullscreenVisibility(true)
+
+        vimeoPlayer.setFullscreenClickListener {
+            var requestOrientation = VimeoPlayerActivity.REQUEST_ORIENTATION_AUTO
+            startActivityForResult(VimeoPlayerActivity.createIntent(this, requestOrientation, vimeoPlayer), REQUEST_CODE)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
+            var playAt = data!!.getFloatExtra(VimeoPlayerActivity.RESULT_STATE_VIDEO_PLAY_AT, 0f)
+            vimeoPlayer.seekTo(playAt)
+
+            var playerState = PlayerState.valueOf(data!!.getStringExtra(VimeoPlayerActivity.RESULT_STATE_PLAYER_STATE))
+            when (playerState) {
+                PlayerState.PLAYING -> vimeoPlayer.play()
+                PlayerState.PAUSED -> vimeoPlayer.pause()
+            }
+        }
     }
 }
+
